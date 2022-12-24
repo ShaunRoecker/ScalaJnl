ada5c8d53182aca269d08e8e891fd41784be8db4
s-roecker <94014218+s-roecker@users.noreply.github.com>
2022-12-23 20:33:55 -0500

    12/23/2022 Scala/FP

--- 2022-Dec/22/SCALA_12_22_2022.scala
+++ 2022-Dec/22/SCALA_12_22_2022.scala
@@ -16,4 +16,42 @@ object SCALA_12_22_2022 extends App {
     /////////////////////////////////////////////////////////
     // 2.5 (pg.209)
   
+
+    val date = raw"(\d{4})-(\d{2})-(\d{2})".r
+    "2004-01-20" match 
+        case date(year, month, day) => println(s"$year was a good year for PLs.")
+
+    // val input = scala.io.StdIn.readLine()
+    // println(s"Input: $input")
+    // println(input.isInstanceOf[String]) //true
+
+    // println("Enter temperature in Celcius: ")
+    // val celcius = scala.io.StdIn.readLine()
+    // val rCelcius = raw"\\A[0-9]+\\z".r
+
+    // if (rCelcius == raw"\\A[0-9]+\\z".r) 
+    //     val celcius2 = celcius.toFloat
+    //     val fahrenheit = (celcius2 * 9 / 5) + 32
+    //     println(s"${celcius} is ${fahrenheit} F\n")
+    // else
+    //     println("Expecting a number")
+
+    val r2 = raw"[0-9]".r
+    val str2 = "12345"
+
+    val matches2 = r2.findAllIn(str2)
+    println(matches2.toList) //List(1, 2, 3, 4, 5)
+
+
+    
+
+
+
+    
+
+
+
+
+
+
 }
