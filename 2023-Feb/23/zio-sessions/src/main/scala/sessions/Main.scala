1701f26f28f566ae179b5abfded4d9c240af20a2
s-roecker <94014218+s-roecker@users.noreply.github.com>
2023-02-23 23:57:12 -0500

    ZIO Sessions/Optics

---
+++ 2023-Feb/23/zio-sessions/src/main/scala/sessions/Main.scala
@@ -0,0 +1,51 @@
+package sessions
+
+import zio._
+import zio.optics._
+
+object MyZIOApp extends ZIOAppDefault {
+    case class Person(name: String)
+
+    object Person {
+        val name: Lens[Person, String] =
+            Lens(
+                person => Right(person.name),
+                newName => person => Right(person.copy(name = newName))
+            )
+    }
+
+    val person = Person("John")
+    val person2 = Person.name.set("Frank")(person)
+    val person3 = Person.name.update(person)(_.toLowerCase)
+
+    case class Name(firstName: FirstName)
+    case class FirstName(value: String)
+
+    object Name {
+        val firstName: Lens[Name, FirstName] =
+            Lens(
+                name => Right(name.firstName),
+                newFirstName => name => Right(name.copy(firstName = newFirstName))
+            )
+        
+        val firstNameVal: Lens[FirstName, String] =
+            Lens(
+                firstName => Right(firstName.value),
+                newValue => firstName => Right(firstName.copy(value = newValue))
+            )
+        val nameToFirstNameVal = firstName >>> firstNameVal
+    }
+
+    val name1 = Name(FirstName("Jane"))
+    val name2 = Name.nameToFirstNameVal.update(name1)(_ + "t")
+    val name3 = Name.nameToFirstNameVal.set("Jane2")(name1)
+
+    def run = for {
+        _ <- ZIO.succeed(person2).debug //Right(Person(Frank))
+        _ <- ZIO.succeed(person3).debug //Right(Person(john))
+        _ <- ZIO.succeed(name1).debug //Name(FirstName(Jane))
+        _ <- ZIO.succeed(name2).debug //Right(Name(FirstName(Janet)))
+        _ <- ZIO.succeed(name3).debug //Right(Name(FirstName(Jane2)))
+
+    } yield ()
+}
