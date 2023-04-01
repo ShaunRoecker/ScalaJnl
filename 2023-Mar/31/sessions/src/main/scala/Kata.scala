1e8c637335a253f20c717c6bb5d80027e5c17593
s-roecker <94014218+s-roecker@users.noreply.github.com>
2023-04-01 11:48:29 -0400

    katas

--- 2023-Mar/31/sessions/src/main/scala/Kata.scala
+++ 2023-Mar/31/sessions/src/main/scala/Kata.scala
@@ -46,10 +46,125 @@ object Kata extends App {
    println(phoneString("(923) 667-3345")) // List(9, 2, 3, 6, 6, 7, 3, 3, 4, 5)
    println(phoneString("974-334-9856")) // List(9, 7, 4, 3, 3, 4, 9, 8, 5, 6)
 
+   val nemo = Vector("nemo")
+
+   val x3 = for {
+      i <- nemo
+      if (i == "nemo")
+   } do println("found Nemo")
+
+   def findNemo(vect: Vector[String]): Unit = 
+      for {
+         i <- nemo
+         if (i == "nemo")
+      } do println("found Nemo")
+
+   findNemo(nemo)
+
+   // Given an array of integers, return the indices of the 
+   // two numbers that add up to a given target
    
+   import scala.collection.immutable.HashMap
+
+   def twoSum(nums: Array[Int], target: Int): Array[Int] = {
+      val indexByValue = HashMap(nums.zipWithIndex: _*)
+      val result = nums.indices.collectFirst(Function.unlift { i =>
+         indexByValue.get(target - nums(i)).filter(_ != i).map(Array(i, _))
+      })
+      result.getOrElse(Array(-1, -1))
+   }
+
+   println(twoSum(Array(1, 2, 3, 4, 5, 6, 7), 7).toList)
+
+   def twoSum2(nums: Array[Int], target: Int): Array[Int] = {
+      nums.indices.iterator
+         .scanLeft(HashMap.empty[Int, Int]) { (map, i) =>
+            map + (nums(i) -> i)
+         }
+         .zipWithIndex
+         .collectFirst(Function.unlift { case (indexByValue, i) =>
+            indexByValue.get(target - nums(i)).filter(_ != i).map(Array(i, _))
+         })
+         .getOrElse(Array(-1, -1))
+   }
+   def twoSum3(nums: Array[Int], target: Int) =
+      nums.indices
+
+   println(twoSum2(Array(1, 2, 3, 4, 5, 6, 7), 4).toList)
+   
+   println("hh".getClass()) //class java.lang.String
+
+   println("779".reverse.padTo(10, "0").reverse) //Vector(0, 0, 0, 0, 0, 0, 0, 7, 7, 9)
+   // If you use a String as the pad value, you get back a Vector by default
+
+   println("1234".reverse.padTo(7, '0').reverse) // 0001234
+   // If you use a Char as the pad value, you get back a Vector by default
+
+   // You have to use the double reverse to get the pad values to the left.
+
+   println("aloha".padTo(10,'a')) //  alohaaaaaa
+
+   println(List(1, 2, 3).padTo(0, 10)) // doesnt work intuitively - List(1, 2, 3)
+
+   // To get this functionality
+   println{
+      val arr = List(1, 2, 3)
+      arr ++ List.fill(10 - arr.length)(0)
+   }
+   List(1, 2, 3, 0, 0, 0, 0, 0, 0, 0)
 
    
 
+   def twoSum4(nums: Array[Int], target: Int): Option[(Int, Int)] = {
+      Iterator.range(0, nums.length).map { i =>
+            Iterator.range((i + 1), nums.length).collectFirst {
+               case j if ((nums(i) + nums(j)) == target) => (i, j)
+            }
+         }.collectFirst {
+            case Some(tuple) => tuple
+         }
+   }
+
+   println(twoSum4(Array(1, 2, 3, 4, 5, 6, 7), 9))
+
+
+   def twoSum5(nums: List[Int], target: Int) = 
+      Iterator.range(0, nums.length).map { i =>
+         Iterator.range(i + 1, nums.length).collectFirst {
+            case j if ((nums(i) + nums(j)) == target) => (i, j)
+         }
+      }  // out at this point: List(None, Some((1,6)), Some((2,5)), Some((3,4)), None, None, None)
+      .collectFirst {
+         case Some(x) => x
+      } // out at this point: Some((1,6))
+         
+
+   println(twoSum5(List(1, 2, 3, 4, 5, 6, 7), 9))
+
+    
+
+   def twoSum6(nums: List[Int], target: Int) =
+      Iterator.range(0, nums.length).map { i =>
+         Iterator.range(i + 1, nums.length).collectFirst {
+            case j if ((nums(i) + nums(j)) == target) => (i, j)
+         }
+      }
+   
+   println(twoSum6(List(1, 2, 3, 4, 5, 6, 7), 9).toList)
+
+   def twoSum7(nums: List[Int], target: Int) =
+      Iterator.range(0, nums.length).map { i =>
+         Iterator.range(i + 1, nums.length).collectFirst {
+            case j if ((nums(i) + nums(j)) == target) => (i, j)
+         }}
+      // }.collectFirst{
+      //    case Some(tuple) => tuple
+      // }
+   
+   println(twoSum7(List(1, 2, 3, 4, 5, 6, 7), 9).toList)
+
    
 
-}
+
+
+}  
