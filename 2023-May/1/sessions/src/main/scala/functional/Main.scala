f7e60b6dc2f996132d6a0e6209762afb44d30517
s-roecker <shaun.roecker@gmail.com>
2023-05-02 01:06:35 -0400

    fpinscala list2

--- 2023-May/1/sessions/src/main/scala/functional/Main.scala
+++ 2023-May/1/sessions/src/main/scala/functional/Main.scala
@@ -8,10 +8,18 @@ object Program:
     def main =
         println("Functional Data Structures")
 
-        val list1 = List(1,2,3)
+        val list1 = List(1,2,3,4,5,6,7)
         println(list1.sum) // 6
         println(list1.product) // 6
         println(List(1,2,3) ++ List(4,5,6))
         println(List(1,2,3).append(List(4,5,6)))
+        // val list2: List[Double] = Nil // The data constructor Nil, since it doesn't contain any
+        // // elements, can be used to construct a List of whatever type we want
+        println(list1.tail)
+        println(list1.setHead(1000))
+        println(list1.drop(2))
+        println(list1.init)
+        println(list1.sum2)
+
 
 
--- 2023-May/1/sessions/src/main/scala/functional/datastructures/List.scala
+++ 2023-May/1/sessions/src/main/scala/functional/datastructures/List.scala
@@ -30,14 +30,82 @@ object List:
                     case Cons(h, t) => loop(t, h * acc)
             loop(as, 1)
 
-    extension[A](first: List[A])
-        def ++(second: List[A]): List[A] =
-            first match
-                case Nil => second
-                case Cons(h, t) => Cons(h, t ++ second)
+        def sum2: Int =
+            as.foldRight(0)(_ + _)
+
+        def product2: Int =
+            as.foldRight(1)(_ * _)
+
+    extension[A](xs: List[A])
+        def ++(xs2: List[A]): List[A] =
+            xs match
+                case Nil => xs2
+                case Cons(h, t) => Cons(h, t ++ xs2)
         
-        def append(second: List[A]): List[A] =
-            first ++ second
+        def append(xs2: List[A]): List[A] =
+            xs ++ xs2
+
+        def tail: List[A] =
+            xs match
+                case Nil => sys.error("can't call tail on empty list")
+                case Cons(_, t) => t
+
+        def setHead(head: A): List[A] =
+            xs match
+                case Nil => sys.error("can't call setHead on empty list")
+                case Cons(_, t) => Cons(head, t)
+
+        def drop(n: Int): List[A] =
+            if (n <= 0) xs
+            else xs match
+                case Nil => Nil
+                case Cons(_, t) => tail.drop(n-1)
+
+        def dropWhile(p: A => Boolean): List[A] =
+            xs match
+                case Cons(h, t) if p(h) => t.dropWhile(p)
+                case _ => xs
+
+        def init: List[A] =
+            xs match
+                case Nil => sys.error("init on empty list")
+                case Cons(_, Nil) => Nil
+                case Cons(h, t) => Cons(h, t.init)
+            
+        def initFaster: List[A] =
+            import scala.collection.mutable.ListBuffer
+            val buf = new ListBuffer[A]
+            @annotation.tailrec
+            def loop(curr: List[A]): List[A] =
+                curr match
+                    case Nil => sys.error("init on empty list")
+                    case Cons(_, Nil) => List(buf.toList*)
+                    case Cons(h, t) => 
+                        buf += h;
+                        loop(t)
+            loop(xs) 
+
+        def foldRight[B](acc: B)(f: (A, B) => B): B =
+            xs match
+                case Nil => acc
+                case Cons(h, t) => f(h, t.foldRight(acc)(f))
+
+        
+
+        
+
+        
+                    
+
+
+
+
+        
+
+        
+
+        
+            
 
    
         
